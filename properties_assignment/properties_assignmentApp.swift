9aca49d4843dd8fea52db080ed11aaddd407f50a
Oscar Karlsson <oscar@starmony.io>
2023-04-17 15:18:11 +0200

    Initial Commit

---
+++ properties_assignment/properties_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  properties_assignmentApp.swift
+//  properties_assignment
+//
+//  Created by Oscar Karlsson on 2023-04-17.
+//
+
+import SwiftUI
+
+@main
+struct properties_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
